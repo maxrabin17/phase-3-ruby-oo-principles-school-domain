b8844a90d3ead312007dbb5fb69e8ae7f03473e6
maxrabin <max.rabin17@gmail.com>
2021-06-29 12:30:46 -0400

    commit tree 526b474eaaf03662347a212f2c59f688065ae9f4

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,28 @@
 # code here!
+require 'pry'
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(name = nil)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student_name, grade)
+        roster[grade] ||= []
+        roster[grade] << student_name 
+    end
+    # binding.pry
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        sorted_students = {}
+        roster.each do |grade, student|
+            sorted_students[grade] = student.sort
+        end
+        sorted_students
+    end
+end
